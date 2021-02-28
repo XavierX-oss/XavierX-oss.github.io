28cf28fce00eabd8b04d96deb394bce35fe8a98b
XavierX-oss <suryahalim928@gmail.com>
2021-02-28 16:53:34 +0700

    initial commit

--- suit.js
+++ suit.js
@@ -3,40 +3,35 @@ var req = true;
 while ( req ) {
 
     // Menangkap pilihan player
-    var player = prompt('Pilihlah antara: semut, gajah, orang');
+    var player = prompt('Pilihlah antara: gunting, batu, kertas');
 
     // Menangkap pilihan komputer
     // Memanggil bilangan acak atau random
     var comp = Math.random();
 
     if (comp < 0.30) {
-        comp = 'gajah';
+        comp = 'batu';
     } else if (comp >= 0.30 && comp < 0.60) {
-        comp = 'orang';
+        comp = 'kertas';
     } else{
-        comp = 'semut';
+        comp = 'gunting';
     }
 
     // Menentukan peraturan / rules
     var hasil = '';
     if (player == comp) {
         hasil = 'Seri!';
-    } else if (player == 'gajah') {
-        if (comp == 'orang') {
+    } else if (player == 'batu') {
+        if (comp == 'kertas') {
             hasil = 'Menang!';
         } else {
             hasil = 'Kalah!';
         }
-    } else if (player == 'orang') {
-        /* if (comp == 'gajah') {
-            hasil = 'Kalah';
-        } else {
-            hasil = 'Menang'
-        } */
+    } else if (player == 'kertas') {
         // Ternary atau versi pendek
-        hasil = (comp == 'gajah') ? 'Kalah' : 'Menang';
-    } else if (player == 'semut') {
-        hasil = (comp == 'orang') ? 'Kalah' : 'Menang';
+        hasil = (comp == 'batu') ? 'Kalah' : 'Menang';
+    } else if (player == 'gunting') {
+        hasil = (comp == 'kertas') ? 'Kalah' : 'Menang';
     } else {
         hasil = 'Memasukkan pilihan yang salah!';
     }
